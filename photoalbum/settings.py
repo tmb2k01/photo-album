092c5ab2d2cad86ef46973aa1d527966b7f3ccb1
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2025-03-11 22:52:04 +0100

    Add OpenShift to ALLOWED_HOSTS

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_z*_6aadnir9sq=jut3#sv!3i+exnjj9!2+*ajm@616_=cv1ig"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '*.openshiftapps.com']
 
 
 # Application definition
