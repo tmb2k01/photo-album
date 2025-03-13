4a1932f1a5ac8fcf3ba3f114db60c8001f54e34f
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2025-03-13 21:33:56 +0100

    Set DB information

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "photoalbum.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "database-name",
+        "USER": "database-user",
+        "PASSWORD": "database-password",
+        "HOST": "172.30.237.74",
+        "PORT": "5432",
     }
 }
 
